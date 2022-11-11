57f45e7953bf8fb97c9577cf90d3f3d141694a53
rodhanter <45400482+rodhanter@users.noreply.github.com>
2022-11-11 12:29:07 -0300

    Update videoService.js

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const PROJECT_URL = "https://okstarojudwxmjdcuhea.supabase.co";
-const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rc3Rhcm9qdWR3eG1qZGN1aGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ4OTU1OTUsImV4cCI6MTk4MDQ3MTU5NX0.shktap50ImF9SqhqcsugSpg38trqKe7aNEP4RvhpQE0";
+const PROJECT_URL = "https://jihzrlucbhsknfrnlzeh.supabase.co";
+const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppaHpybHVjYmhza25mcm5semVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxNjMyMTcsImV4cCI6MTk4MzczOTIxN30.0sjh8GKGY3_nG-VOmdHtUTCR3rXvgq2HSIfUthkDZDI";
 const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
 
 export function videoService() {
